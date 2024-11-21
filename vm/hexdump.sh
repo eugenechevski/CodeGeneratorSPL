5c15f80abbc38f324bb5fb93455c9a7e9d55c150
MarkMango21 <mark2asf@gmail.com>
2024-11-20 19:49:54 -0800

    ok

--- vm/hexdump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# dump the given file in hex, with a big-endian format
-od --endian=big -t x4 "$@"
--- vm/test0.asm
+++
@@ -1,6 +0,0 @@
-# $Id: test0.asm,v 1.2 2023/09/16 13:45:07 leavens Exp $
-.text start
-start: EXIT
-.data 12
-.stack 40
-.end
